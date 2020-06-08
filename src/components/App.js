4a622af0742e1478e449cd67ae928f4c5b79d0c8
Marcus Schreiter <marcus.schreiter@meso.design>
2020-06-08 16:22:27 +0200

    make fullscreen

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/index.css';
 
 const App = () => {
-  return <div className="bg-alpha font-body">Hello world</div>;
+  return <div className="bg-alpha font-body h-full w-full">Hello world</div>;
 };
 
 export default App;
