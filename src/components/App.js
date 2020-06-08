7f8d57f3600c6d911b6ddb14f3e8bd12202eae26
Marcus Schreiter <marcus.schreiter@meso.design>
2020-06-08 16:30:21 +0200

    center text

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import '../styles/index.css';
 
 const App = () => {
-  return <div className="bg-alpha font-body h-full w-full">Hello world</div>;
+  return (
+    <div className="bg-alpha font-body h-full w-full flex justify-center items-center text-lg">
+      Hello world
+    </div>
+  );
 };
 
 export default App;
