aff9f8019e5bd42918bc2ee9e946d07b550ff856
Marcus Schreiter <marcus.schreiter@meso.design>
2020-06-08 15:16:52 +0200

    add tailwindcss

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../styles/index.css';
 
 const App = () => {
-  return <div>Hello world</div>;
+  return <div className="bg-gray-400">Hello world</div>;
 };
 
 export default App;
